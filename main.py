7f9b2354a2b7dac130cb13c60c2a491d7e9c1e92
wokwak <wokwak@hotmail.com>
2021-06-15 12:06:12 +0700

    Update 3

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import streamlit as st
-# import streamlit.components.v1 as components
+import pandas as pd
+import numpy as np
+import pickle
 
+# Header of page
+st.header('Car residual values prediction')
 
-# st.text('This will appear first')
-print("hello world")
+#Sub-header of page
+st.subheader('The Residual Values')
+
+# Body paragraph
+st.write(
+    """
+    The Residual Values (RV) model is a model to predict car values for given times. There are several car conditions having
+    effects to future prices. In the model, transformed categorical features via One-Hot Encoding and scaled numeric
+    features via normalization and polynomial transformer. The core model is used CatBoost regression.
+    """
+)
+
+st.subheader('To use model prediction, please following below steps:')
+
+st.write(
+    """
+    1. From the left side of this page, there is an area to input several car conditions. \n
+    2. To input car conditions that needed to be predicted. \n
+    3. See the results below.
+    """
+)
